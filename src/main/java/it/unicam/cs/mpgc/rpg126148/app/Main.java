07d4adc482e71437cbfb368f1b8a90a9b280d377
Christian Taccari <christian.taccari@studenti.unicam.it>
2026-05-12 12:05:42 +0200

    Set up iniziale: package e classe main di test per gradle

---
+++ src/main/java/it/unicam/cs/mpgc/rpg126148/app/Main.java
@@ -0,0 +1,12 @@
+package it.unicam.cs.mpgc.rpg126148.app;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Jujutsu RPG started!");
+
+    }
+}
